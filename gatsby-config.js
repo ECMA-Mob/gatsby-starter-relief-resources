6fc62cc4d328f1ae22c316cceb0957cd6550564f
Henry Quinn <hquinn@clarityssi.com>
2020-03-24 12:07:41 -0400

    First commit.

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-relief-resources`,
+      options: {
+        authorName: `Henry Quinn`,
+        authorLink: `https://henryneeds.coffee`,
+      },
+    },
+  ],
+}
